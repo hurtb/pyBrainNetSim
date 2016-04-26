194ff716790b8cbf1677e6d6496c94fe4b9af25d
Brian Hurt <cubnb1@yahoo.com>
2016-04-26 01:33:49 -0600

    add initial sphinx docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # pyBrainNetSim documentation build configuration file, created by
-# sphinx-quickstart on Mon Apr 25 23:56:34 2016.
+# sphinx-quickstart on Tue Apr 26 01:13:44 2016.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -32,11 +32,7 @@ import shlex
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -109,7 +105,7 @@ pygments_style = 'sphinx'
 #keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
@@ -290,7 +286,3 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -1,5 +1,5 @@
 .. pyBrainNetSim documentation master file, created by
-   sphinx-quickstart on Mon Apr 25 23:56:34 2016.
+   sphinx-quickstart on Tue Apr 26 01:13:44 2016.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
